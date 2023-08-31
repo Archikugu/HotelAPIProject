a90b34de3953f6b5fb4227aa4b65570efaccd14f
Gokhan <engokhangok@gmail.com>
2023-08-31 19:27:16 +0300

    Chapter 7 Complete (Added Identity & Register and Login Page)

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,6 +1,11 @@
+using HotelProject.DataAccess.Concrete;
+using HotelProject.Entities.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 builder.Services.AddAutoMapper(typeof(HotelProject.WebUI.Mapping.AutoMapperConfig));
